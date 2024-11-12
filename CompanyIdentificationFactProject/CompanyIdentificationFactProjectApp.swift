2ee70a3ef3a58987c3938b2c6139882c226c59c8
javaad.ali.ny@gmail.com <javaad101>
2024-11-11 21:35:45 -0600

    Initial Commit

---
+++ CompanyIdentificationFactProject/CompanyIdentificationFactProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompanyIdentificationFactProjectApp.swift
+//  CompanyIdentificationFactProject
+//
+//  Created by AdvocatesClose on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CompanyIdentificationFactProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
